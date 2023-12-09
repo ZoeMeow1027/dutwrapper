0b5b9010f524196e1b0ff372de3bcd7ff47dfbaa
Quế Tùng <ZoeMeow1027@gmail.com>
2023-12-10 03:11:09 +0700

    Update UtilsTest.java

--- src/test/java/io/dutwrapper/dutwrapper/UtilsTest.java
+++ src/test/java/io/dutwrapper/dutwrapper/UtilsTest.java
@@ -2,11 +2,15 @@ package io.dutwrapper.dutwrapper;
 
 import org.junit.jupiter.api.Test;
 
+import io.dutwrapper.dutwrapper.model.utils.DutSchoolYearItem;
+
 public class UtilsTest {
     @Test
     void finalTest() throws Exception {
         Long currentUnix = Utils.getCurrentTimeInUnix();
         System.out.println(currentUnix);
+
+        DutSchoolYearItem test = Utils.getCurrentSchoolWeek();
 
         System.out.println(Utils.getCurrentSchoolWeek().toString());
     }
