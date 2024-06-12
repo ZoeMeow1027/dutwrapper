e32e1b8170f303920929138e44e3b119fbf7bd52
Quế Tùng <47247560+ZoeMeow1027@users.noreply.github.com>
2024-06-12 23:16:30 +0700

    Bumped to 1.10.0

--- src/test/java/io/dutwrapper/dutwrapper/AccountTest.java
+++ src/test/java/io/dutwrapper/dutwrapper/AccountTest.java
@@ -4,110 +4,39 @@ import org.junit.jupiter.api.Test;
 
 import com.google.gson.Gson;
 
-import io.dutwrapper.dutwrapper.model.accounts.AccountInformation;
-import io.dutwrapper.dutwrapper.model.accounts.SubjectFeeItem;
-import io.dutwrapper.dutwrapper.model.accounts.SubjectScheduleItem;
-import io.dutwrapper.dutwrapper.model.accounts.trainingresult.AccountTrainingStatus;
-
-import java.io.IOException;
-import java.util.ArrayList;
-
-class AccountTest {
-    static String initialize() throws IOException {
-        HttpClientWrapper.Response response = Account.getSessionId();
-        return response.getSessionId();
-    }
-
-    static void login(String sessionId, String user, String pass) throws Exception {
-        Account.login(
-                sessionId,
-                user,
-                pass);
-
-        if (Account.isLoggedIn(sessionId)) {
-            System.out.println("Logged in!");
-        } else
-            throw new Exception("This Session ID hasn't logged in!");
-    }
-
-    static void getSubjectSchedule(String sessionId, Integer year, Integer semester) throws Exception {
-        ArrayList<SubjectScheduleItem> subjectScheduleList = Account.getSubjectSchedule(sessionId, year, semester);
-        for (SubjectScheduleItem subjectScheduleItem: subjectScheduleList) {
-            System.out.println(String.format(
-                "Subject #%d\nName: %s\nID: %s\n========================",
-                subjectScheduleList.indexOf(subjectScheduleItem),
-                subjectScheduleItem.getName(),
-                subjectScheduleItem.getId()
-            ));
-        }
-    }
-
-    static void getSubjectFee(String sessionId, Integer year, Integer semester) throws Exception {
-        ArrayList<SubjectFeeItem> subjectFeeList = Account.getSubjectFee(sessionId, year, semester);
-        for (SubjectFeeItem subjectFeeItem: subjectFeeList) {
-            System.out.println(String.format(
-                "Subject #%d\nName: %s\nID: %s\n========================",
-                subjectFeeList.indexOf(subjectFeeItem),
-                subjectFeeItem.getName(),
-                subjectFeeItem.getId()
-            ));
-        }
-    }
-
-    static void getAccountInformation(String sessionId) throws Exception {
-        AccountInformation accInfo = Account.getAccountInformation(sessionId);
-        System.out.println(new Gson().toJson(accInfo));
-    }
-
-    static void getAccountTrainingStatus(String sessionId) throws Exception {
-        AccountTrainingStatus accountTrainingStatus = Account.getAccountTrainingStatus(sessionId);
-        System.out.println(new Gson().toJson(accountTrainingStatus));
-    }
-
-    static void logout(String sessionId) throws Exception {
-        Account.logout(sessionId);
-
-        if (!Account.isLoggedIn(sessionId)) {
-            System.out.println("Logged out!");
-        } else
-            throw new Exception("This Session ID hasn't logged out yet!");
-    }
-
+public class AccountTest {
     @Test
-    void finalTest() throws Exception {
-        String sessionId;
-
-        String env = System.getenv("dut_account");
-
-        if (env == null)
+    void test() throws Exception {
+        String envDutAccount = System.getenv("dut_account");
+        if (envDutAccount == null)
             throw new Exception("No account found! Please define \"dut_account\" variable to test account library.");
+        if (envDutAccount.split("\\|").length != 2)
+            throw new Exception("\"dut_account\" variable isn't define correctly! Please follow format username|password.");
+        String user = envDutAccount.split("\\|")[0];
+        String pass = envDutAccount.split("\\|")[1];
+        Integer year = 20;
+        Integer semester = 2;
 
-        String user = env.split("\\|")[0];
-        String pass = env.split("\\|")[1];
+        Account.Session session = Account.getSession();
+        Account.AuthInfo authInfo = new Account.AuthInfo(user, pass);
+        Gson gson = new Gson();
 
-        Integer year = 22;
-        Integer semester = 1;
-
-        // Initialize Session ID
-        sessionId = initialize();
-        System.out.println(sessionId);
-
-        // Login
-        login(sessionId, user, pass);
-
-        // Subject schedule
-        getSubjectSchedule(sessionId, year, semester);
-
-        // Subject fee
-        getSubjectFee(sessionId, year, semester);
-
-        // Account information
-        getAccountInformation(sessionId);
-
-        // Account training status
-        getAccountTrainingStatus(sessionId);
-
-        // Logout
-        logout(sessionId);
+        System.out.println("Create new Session");
+        System.out.println(gson.toJson(session));
+        System.out.println(gson.toJson(Account.isLoggedIn(session)));
+        System.out.println("\nLogin and check again");
+        Account.login(session, authInfo);
+        System.out.println(gson.toJson(Account.isLoggedIn(session)));
+        System.out.println("\nFetch subject schedule list");
+        System.out.println(gson.toJson(Account.fetchSubjectSchedule(session, year, semester)));
+        System.out.println("\nFetch subject fee list");
+        System.out.println(gson.toJson(Account.fetchSubjectFee(session, year, semester)));
+        System.out.println("\nFetch account information");
+        System.out.println(gson.toJson(Account.fetchAccountInformation(session)));
+        System.out.println("\nFetch account training status");
+        System.out.println(gson.toJson(Account.fetchAccountTrainingStatus(session)));
+        System.out.println("\nLogout and check if logged out");
+        Account.logout(session);
+        System.out.println(gson.toJson(Account.isLoggedIn(session)));
     }
 }
